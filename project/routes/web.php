5bb504eec0c03b600bfe35dcb90b285744b9e843
danielplebani <danielpleba03@gmail.com>
2023-10-31 16:09:25 +0100

    Milestone 1
    Tramite gli appositi comandi artisan create un model con relativa migration e un resource controller

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController;
+use App\Http\Controllers\Guest\PageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [PageController::class, 'index'])->name('home');
 
-
+Route::resource('comic', ComicController::class);
