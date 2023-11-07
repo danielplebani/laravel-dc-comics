170dc27d97f17ad562ee25bf432d28c8629e5e21
danielplebani <danielpleba03@gmail.com>
2023-11-07 17:57:58 +0100

    a

--- project/database/migrations/2023_10_31_145344_create_comics_table.php
+++ project/database/migrations/2023_10_31_145344_create_comics_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-
             $table->string('title');
             $table->longText('description')->nullable();
             $table->longText('thumb');
