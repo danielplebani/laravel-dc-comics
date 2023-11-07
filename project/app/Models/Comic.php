c72bdf76429843a426f013350b2e2572466b3479
danielplebani <danielpleba03@gmail.com>
2023-11-07 17:57:27 +0100

    last changes

--- project/app/Models/Comic.php
+++ project/app/Models/Comic.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
     use HasFactory;
+    //use SoftDeletes;
 
     protected $fillable = ['title', 'description', 'thumb', 'price', 'series', 'type', 'writers', 'artists', 'sale_date'];
 
